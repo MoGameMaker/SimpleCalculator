83624ed684514eb5c0a535ef1e616ff70b8cb8a1
MoGameMaker <161384554+MoGameMaker@users.noreply.github.com>
2024-03-04 19:14:19 -0400

    modified:   calculator_1.2.js 	modified:   index.html 	modified:   index_1.1.html 	modified:   index_1.2.html 	modified:   style_1.2.css

--- calculator_1.2.js
+++ calculator_1.2.js
@@ -33,30 +33,60 @@ buttons.forEach(button => {
 
 document.getElementById('textsaddle').addEventListener('click', function() {
     document.body.style.backgroundColor = 'saddlebrown';
-    document.getElementById('textlawn').style.backgroundColor = '#999';
-    document.getElementById('textaqua').style.backgroundColor = '#999';
+    document.getElementById('textlawn').style.backgroundColor = 'saddlebrown';
+    document.getElementById('textaqua').style.backgroundColor = 'saddlebrown';
+    document.getElementById('textsaddle').style.backgroundColor = 'saddlebrown';
+    document.getElementById('textchocolate').style.backgroundColor = 'saddlebrown';
+    document.getElementById('textgolden').style.backgroundColor = 'saddlebrown';
+    document.getElementById('textbluev').style.backgroundColor = '#999';
 });
 
 document.getElementById('textaqua').addEventListener('click', function() {
     document.body.style.backgroundColor = 'aquamarine';
     document.getElementById('textlawn').style.backgroundColor = '#999';
+    document.getElementById('textbluev').style.backgroundColor = 'aquamarine';
     document.getElementById('textaqua').style.backgroundColor = 'aquamarine';
+    document.getElementById('textsaddle').style.backgroundColor = 'aquamarine';
+    document.getElementById('textchocolate').style.backgroundColor = 'aquamarine';
+    document.getElementById('textgolden').style.backgroundColor = 'aquamarine';
 });
 
 document.getElementById('textlawn').addEventListener('click', function() {
     document.body.style.backgroundColor = 'lawngreen';
     document.getElementById('textaqua').style.backgroundColor = '#999';
+    document.getElementById('textbluev').style.backgroundColor = 'lawngreen';
     document.getElementById('textlawn').style.backgroundColor = 'lawngreen';
+    document.getElementById('textsaddle').style.backgroundColor = 'lawngreen';
+    document.getElementById('textchocolate').style.backgroundColor = 'lawngreen';
+    document.getElementById('textgolden').style.backgroundColor = 'lawngreen';
 });
 
 document.getElementById('textgolden').addEventListener('click', function() {
     document.body.style.backgroundColor = 'goldenrod';
-    document.getElementById('textlawn').style.backgroundColor = '#999';
-    document.getElementById('textaqua').style.backgroundColor = '#999';
+    document.getElementById('textlawn').style.backgroundColor = 'goldenrod';
+    document.getElementById('textaqua').style.backgroundColor = 'goldenrod';
+    document.getElementById('textsaddle').style.backgroundColor = 'goldenrod';
+    document.getElementById('textbluev').style.backgroundColor = 'goldenrod';
+    document.getElementById('textchocolate').style.backgroundColor = 'goldenrod';
+    document.getElementById('textgolden').style.backgroundColor = 'goldenrod';
 });
 
 document.getElementById('textchocolate').addEventListener('click', function() {
     document.body.style.backgroundColor = 'chocolate';
-    document.getElementById('textlawn').style.backgroundColor = '#999';
-    document.getElementById('textaqua').style.backgroundColor = '#999';
+    document.getElementById('textlawn').style.backgroundColor = 'chocolate';
+    document.getElementById('textaqua').style.backgroundColor = 'chocolate';
+    document.getElementById('textsaddle').style.backgroundColor = 'chocolate';
+    document.getElementById('textbluev').style.backgroundColor = 'chocolate';
+    document.getElementById('textchocolate').style.backgroundColor = 'chocolate';
+    document.getElementById('textgolden').style.backgroundColor = 'chocolate';
+});
+
+document.getElementById('textbluev').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'blueviolet';
+    document.getElementById('textsaddle').style.backgroundColor = '#999';
+    document.getElementById('textaqua').style.backgroundColor = 'blueviolet';
+    document.getElementById('textlawn').style.backgroundColor = 'blueviolet';
+    document.getElementById('textbluev').style.backgroundColor = 'blueviolet';
+    document.getElementById('textchocolate').style.backgroundColor = 'blueviolet';
+    document.getElementById('textgolden').style.backgroundColor = 'blueviolet';
 });
