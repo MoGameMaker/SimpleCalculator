81dcc699079b1ba3afe8fb67c355a1769279755c
MoGameMaker <161384554+MoGameMaker@users.noreply.github.com>
2024-03-04 18:45:19 -0400

    deleted:    aquamarine_1.0.css 	modified:   calculator_1.0.js 	modified:   calculator_1.1.js 	deleted:    chocolate_1.0.css 	deleted:    chocolate_1.0.html 	deleted:    chocolate_1.1.css 	deleted:    chocolate_1.1.html 	deleted:    golden_1.0.css 	deleted:    golden_1.0.html 	deleted:    golden_1.1.css     deleted:    golden_1.1.html 	renamed:    mv.html -> index.html 	modified:   index_1.0.html 	modified:   index_1.1.html 	deleted:    lawn_1.0.html 	deleted:    lawn_1.1.css 	deleted:    lawn_1.1.html 	deleted:    saddle_1.0.css 	deleted:    saddle_1.0.html 	deleted:    saddle_1.1.css 	deleted:    saddle_1.1.html 	renamed:    lawn_1.0.css -> style_1.0.css 	renamed:    aquamarine_1.1.css -> style_1.1.css

--- calculator_1.1.js
+++ calculator_1.1.js
@@ -30,3 +30,33 @@ buttons.forEach(button => {
         operation(buttonValue);
     });
 });
+
+document.getElementById('textsaddle').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'saddlebrown';
+    document.getElementById('textlawn').style.backgroundColor = '#999';
+    document.getElementById('textaqua').style.backgroundColor = '#999';
+});
+
+document.getElementById('textaqua').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'aquamarine';
+    document.getElementById('textlawn').style.backgroundColor = '#999';
+    document.getElementById('textaqua').style.backgroundColor = 'aquamarine';
+});
+
+document.getElementById('textlawn').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'lawngreen';
+    document.getElementById('textaqua').style.backgroundColor = '#999';
+    document.getElementById('textlawn').style.backgroundColor = 'lawngreen';
+});
+
+document.getElementById('textgolden').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'goldenrod';
+    document.getElementById('textlawn').style.backgroundColor = '#999';
+    document.getElementById('textaqua').style.backgroundColor = '#999';
+});
+
+document.getElementById('textchocolate').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'chocolate';
+    document.getElementById('textlawn').style.backgroundColor = '#999';
+    document.getElementById('textaqua').style.backgroundColor = '#999';
+});
